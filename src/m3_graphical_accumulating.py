71a456fed6f74211e6bacbd472df59984bced7e7
davidsgc <davidsgc@rose-hulman.edu>
2019-01-31 01:13:28 -0500

    finished module 2 and started 3

--- src/m3_graphical_accumulating.py
+++ src/m3_graphical_accumulating.py
@@ -9,8 +9,8 @@ Additionally, it emphasizes that you must
 before you can implement a solution to the problem in Python.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher, Mark Hays,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         Aaron Wilkin, their colleagues, and Gabby Davidson.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -109,7 +109,18 @@ def draw_parallel_lines(n, point, length, window):
     #          ** FIRST DO A CONCRETE EXAMPLE BY HAND! **
     ###########################################################################
     # -------------------------------------------------------------------------
+    x = point.x
+    y = point.y
+    for k in range(n):
+        start = rg.Point(x,y)
+        end = rg.Point(x + length, y)
+        line = rg.Line(start, end)
 
+        line.attach_to(window)
+        
+        y = y + 30
+
+    window.render()
 
 def run_test_draw_lines():
     """ Tests the   draw_lines  function. """
